4da8db16746746e385e12af2e7f189c4f2b1ee3e
Jack Hale <mail@jackhale.co.uk>
2016-03-20 10:57:06 +0100

    Remove stable-ppa, trigger rebuild.

--- dockerfiles/test-builder.sh
+++ dockerfiles/test-builder.sh
@@ -4,8 +4,8 @@
 # to bitbucket for automatic building by quay.io.
 
 # Note: These images do need to be built in a particular order!
-# dev-env-tpetra takes so long to compile, I have left it out.
-for image in base dev-env-base dev-env stable dev stable-ppa dolfin-adjoint dev-env-py3 dev-py3 dev-env-dbg
+# dev-env-trilinos takes so long to compile, I have left it out.
+for image in base dev-env-base dev-env stable dev dolfin-adjoint dev-env-py3 dev-py3 dev-env-dbg
 do
     cd ${image}
     docker build --tag quay.io/fenicsproject/${image}:latest .
